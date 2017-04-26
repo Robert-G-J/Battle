e1d132e8813374e70c3fb0085afdeeab7f656b7a
Robert Jones <robert.g.jones@gmail.com>
2017-04-26 16:39:02 +0100

    Players can input their names into a web form and see a page displaying who they're playing

--- app.rb
+++ app.rb
@@ -1,16 +1,27 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
+    'player_1_name= ' << session[:player_1_name].inspect
+    'player_2_name= ' << session[:player_2_name].inspect
     erb(:index)
   end
 
   post '/names' do
     p params
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
+
 
 
 run! if app_file == $0
