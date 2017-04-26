e0842bed4198e47a3da40b94c450b29ecd341e22
Robert Jones <robert.g.jones@gmail.com>
2017-04-26 15:28:14 +0100

    Adds play.erb view for rendering a form; Players can enter and submit their names

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    "Testing Infrastructure Working!"
+    erb(:index)
   end
+
+  post '/names' do
+    p params
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
+  end
+
 
 run! if app_file == $0
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,18 @@
 require './app.rb'
 
-feature "testing_infrastructure" do
-  scenario "Home Page" do
+# feature "Testing infrastructure" do
+#   scenario "Can run app and check page content" do
+#     visit "/"
+#     expect(page).to have_content "Testing Infrastructure Working!"
+#   end
+# end
+
+feature "Entering information" do
+  scenario "Players can fill in a name form" do
     visit "/"
-    expect(page).to have_content "Testing Infrastructure Working!"
+    fill_in :player_1_name, with: 'The Donald'
+    fill_in :player_2_name, with: 'God'
+    click_button "Submit"
+    expect(page).to have_content "The Donald vs. God"
   end
 end
