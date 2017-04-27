1d1f5ccf9b2744b634855a3ffff815d52e5967f9
Robert Jones <robert.g.jones@gmail.com>
2017-04-27 16:25:05 +0100

    Refactors Battle to use Player class rather than Sinatra Sessions to store Player names

--- app.rb
+++ app.rb
@@ -2,30 +2,27 @@ require 'sinatra/base'
 require 'player'
 
 class Battle < Sinatra::Base
-  enable :sessions
 
   get '/' do
-    'player_1_name= ' << session[:player_1_name].inspect
-    'player_2_name= ' << session[:player_2_name].inspect
     erb(:index)
   end
 
   post '/names' do
     p params
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    p $player_1 = Player.new(params[:player_1_name])
+    p $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    p @player_1_name = $player_1.name
+    p @player_2_name = $player_2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    p @player_1_name = $player_1.name
+    p @player_2_name = $player_2.name
     erb(:attack)
   end
 
